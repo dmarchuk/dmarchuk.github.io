2be9d05c7fc4d3ff006300527a04d6e426d29913
Daniel Marchuk <dmarchuk@me.com>
2020-05-10 18:52:30 +0200

    Add new memory-game

---
+++ memory-game/precache-manifest.361cdcc86e80dae0b733158d94fb496f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9d8e6fed2b4d7455e62903e14ec951",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "66e86b8d9864c4b81252",
+    "url": "/memory-game/static/js/2.d587bde9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memory-game/static/js/2.d587bde9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b9f8c53f85915bdf6f",
+    "url": "/memory-game/static/js/main.21f41f3c.chunk.js"
+  },
+  {
+    "revision": "a7295d09a3c7d39f3388",
+    "url": "/memory-game/static/js/runtime-main.cd8959cd.js"
+  }
+]);
